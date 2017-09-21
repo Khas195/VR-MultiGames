1cbe7b55b0b54ac35439a78a360f822d1f5a9424
Cao Thanh Tung <tungthanhcao1091995@gmail.com>
2017-09-21 14:52:24 +0700

    add

---
+++ VR-MultiGames/Paintable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Paintable : MonoBehaviour
+{
+    [SerializeField]
+    private string paintableShaderName;
+
+    private Material mat;
+
+    void Start()
+    {
+        var ren = this.GetComponent<Renderer>();
+
+        mat = GetPaintableMaterial(ren);
+        if (mat == null)
+        {
+            enabled = false;
+        }
+
+        drawTexture = mat.GetTexture(PaintableDefinition.DrawOnTextureName);
+
+    }
+
+
+
+    private Material GetPaintableMaterial(Renderer renderer)
+    {
+        for (int i = 0; i < renderer.materials.Length; i++)
+        {
+            if (renderer.materials[i].name.Equals(paintableShaderName))
+            {
+                return renderer.materials[i];
+            }
+        }
+        return null;
+    }
+
+    public void Paint(Vector2 texCoord)
+    {
+    }
+}
