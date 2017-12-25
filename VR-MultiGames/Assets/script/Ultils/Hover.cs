9309abe04cf8d8b13e51b0467bb9b196c0c6eaaa
Cao Thanh Tung <tungthanhcao1091995@gmail.com>
2017-12-25 19:47:17 +0700

    Fill newDesign2

--- VR-MultiGames/Assets/script/Ultils/Hover.cs
+++ VR-MultiGames/Assets/script/Ultils/Hover.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[ExecuteInEditMode]
 public class Hover : MonoBehaviour
 {
     public float speed;
     public float amplitude;
+    [SerializeField]
+    private Vector3 pivotPos;
+
     // Use this for initialization
 	void Start ()
 	{
+	    pivotPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 	    var pos = this.transform.position;
-	    pos.y = pos.y + (Mathf.Sin(Time.time * speed) * amplitude);
+	    pos.y = pivotPos.y + (Mathf.Sin(Time.time * speed) * amplitude);
 	    transform.position = pos;
 	}
 }
